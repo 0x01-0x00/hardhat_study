f4cf7ad29e7b88d5d9ee75c6c0301efc6104096a
zhongtao <ztappleemail@163.com>
2025-10-09 10:29:23 +0800

    feat(solidity):重命名合约并添加函数类型示例
    - 将 SolidityStudy.sol重命名为 FunctionStudy.sol
    - 添加 payable 函数示例 payableFunc- 添加 pure 函数示例 pureFunc- 添加 view 函数示例 viewFunc
    - 添加 senderValue 映射记录发送者值
    - 设置 MSG_SENDER_VALUE_MIN 常量限制最小转账金额
    - 添加 require 检查确保 msg.value 大于最小值

--- contracts/solidity_study/FunctionStudy.sol
+++ contracts/solidity_study/FunctionStudy.sol
@@ -1,20 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-contract SolidityStudy {
+contract FunctionStudy {
+
+    uint256 MSG_SENDER_VALUE_MIN = 1*10**18-1;
+
+    mapping(address  => uint256) public senderValue;
     string name = "SolidityStudy";
     constructor(){
     }
 
+    // payable
+    function payableFunc() public payable{
+        require(msg.value > MSG_SENDER_VALUE_MIN, "msg.value is too small");
+        senderValue[msg.sender] = msg.value;
+    }
+
+    // null
     function nullFunc(string memory _name) public returns(string memory){
         name = _name;
         return name;
     }
 
+    // view
     function viewFunc() public view returns(string memory) {
         return name;
     }
 
+    // pure
     function pureFunc(string memory _name) public pure returns(string memory) {
         // name = _name; error
         return _name;
